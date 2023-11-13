4ad30d76c4abbfbb91dd36678bcdd3406429b45e
Sanju <sanjushusanth@gmail.com>
2023-11-14 01:28:04 +0530

    Data Transfromation completed

--- src/Pipelines/training_pipeline.py
+++ src/Pipelines/training_pipeline.py
@@ -4,9 +4,13 @@ from src.logger import logging
 from src.execption import CustomException
 import pandas as pd
 from src.Components.data_ingestion import DataIngestion
+from src.Components.data_transformation import DataTransformation
 
 
 if __name__=='__main__':
     obj=DataIngestion()
     train_data_path, test_data_path = obj.initiate_data_ingestion()
     print(train_data_path, test_data_path)
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, obj_path= data_transformation.initiate_data_transformation(train_data_path, test_data_path)
